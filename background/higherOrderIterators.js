fb2ea89ee705bd2728604ad9b019a5c939f5710a
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-08-07 03:58:24 +0530

    fix a typo

--- background/higherOrderIterators.js
+++ background/higherOrderIterators.js
@@ -1,5 +1,5 @@
 export const map = mapper => async function*(iterable) {
-  for await (const item of terable) {
+  for await (const item of iterable) {
     yield mapper(item)
   }
 }
@@ -10,7 +10,8 @@ export const filter = predicate => async function*(iterable) {
   }
 }
 
-// this one's behavior is a bit different from the Array's reduce since it yields the accumulator on each item (because your original iterable may never end)
+// this one's behavior is a bit different from the Array's reduce since it yields
+// the accumulator on each item (because your original iterable may never end)
 export const reduce = (reducer, acc) => async function*(iterable) {
   for await (const item of iterable) {
     yield (acc = reducer(acc, item))
