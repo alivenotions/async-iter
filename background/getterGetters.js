27b85014f2173068e568d661d8b197de07d4ded9
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-08-08 16:10:03 +0530

    conversion of a getterGetter to an iterable

--- background/getterGetters.js
+++ background/getterGetters.js
@@ -1,4 +1,4 @@
-function range (left, right) {
+/*function range (left, right) {
   return () => {
     // lazy initialization
     let x = left
@@ -12,10 +12,32 @@ function range (left, right) {
       }
     }
   }
+}*/
+
+// can convert this into an iterable with the same underlying model/logic
+function range (left, right) {
+  return {
+    [Symbol.iterator]: () => {
+      let x = left
+      return {
+        next: () => {
+          if(x <= right) {
+            return { done: false, value: x++ }
+          } else {
+            return { done: true }
+          }
+        }
+      }
+    }
+  }
 }
 
 const getGet = range(10, 2500)
 
-for (let get = getGet(), res = get(); !res.done ; res = get()) {
-  console.log(res.value)
+// for (let get = getGet(), res = get(); !res.done ; res = get()) {
+//   console.log(res.value)
+// }
+
+for (const res of range(10, 2500)) {
+  console.log(res)
 }
